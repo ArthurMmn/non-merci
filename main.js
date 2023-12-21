b88cc9b56c8b340c5e72f3e935ce02cd8797fd32
Arthur Maman <arthur.maman@aworldforus.com>
2023-12-21 09:03:57 +0100

    v2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import quoteFiles from "./quotes.txt?raw";
 
 const quotes = quoteFiles.trim().split("\n");
-let currentQuoteIndex = null;
+let currentQuoteIndex;
 
 const randomQuoteIndex = () => {
   let index = Math.floor(Math.random() * quotes.length);
@@ -20,13 +20,34 @@ const speak = () => {
   speechSynthesis.speak(utterance);
 };
 
-const switchQuote = () => {
+const switchQuote = (index) => {
+  console.log(index)
   document.querySelector("#quote").innerHTML =
-    quotes[randomQuoteIndex(currentQuoteIndex)];
+    quotes[index];
   speak();
 };
 
-document.querySelector("#switch-quote").addEventListener("click", switchQuote);
+const putQuoteLinkInClipboard = () => {
+  const currentUrl = window.location.href.split('?')[0];
+  const quoteUrl = `${currentUrl}?q=${currentQuoteIndex}`;
+  navigator.clipboard.writeText(quoteUrl).then(() => {
+    console.log('Quote link copied to clipboard!');
+  }, (err) => {
+    console.error('Could not copy quote link to clipboard: ', err);
+  });
+  if (navigator.share) {
+    navigator.share({
+      title: document.title,
+      text: quotes[currentQuoteIndex],
+      url: quoteUrl
+    })
+  }
+}
+
+document.querySelector("#switch-quote").addEventListener("click", () => switchQuote(randomQuoteIndex(currentQuoteIndex)));
+document.querySelector("#share").addEventListener("click", putQuoteLinkInClipboard);
 document.querySelector("#quote").addEventListener("click", speak);
 
-switchQuote();
+const searchParams = new URLSearchParams(window.location.search);
+currentQuoteIndex = !searchParams.has("q") ? randomQuoteIndex(-1) :searchParams.get("q");
+switchQuote(currentQuoteIndex);
